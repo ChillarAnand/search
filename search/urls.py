95dba7fb332c49091e56d236ddd6dc40a9c19fe9
chillaranand <anand21nanda@gmail.com>
2017-02-19 13:32:46 +0530

    Removed admin from production

--- search/urls.py
+++ search/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url
-from django.contrib import admin
 
 from document.views import index, search, home
 
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-
-    url(r'^admin/', admin.site.urls),
 
     url(r'^index', index, name='index'),
 
